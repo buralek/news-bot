036f87fb1b3149fa56fba39732f39faad65ed90b
Aleksandr Burashnikov <buralek91@gmail.com>
2023-10-23 22:50:07 +0200

    add 5 core modules

---
+++ news-bot-api/src/main/java/buralek/newsbot/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package buralek.newsbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ news-bot-logic/src/test/java/buralek/newsbot/logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package buralek.newsbot.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
